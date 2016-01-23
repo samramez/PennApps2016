d4560a7c0555334123d8d177135dcea1a03e86ce
Sam Ramezanli <Sam@Jackies-MacBook-Pro.local>
2016-01-23 16:22:22 -0500

    Initial commit

---
+++ app/src/main/java/com/yiptv/sam/capitalone/Models/CustomerModel.java
@@ -0,0 +1,19 @@
+package com.yiptv.sam.capitalone.Models;
+
+import android.location.Address;
+
+/**
+ * Created by Sam on 1/23/16.
+ */
+public class CustomerModel {
+
+    public String city;
+    public String streetNumber;
+    public String streetName;
+    public String state;
+    public String zip;
+    public String Id;
+    public Address address;
+    public String firstName;
+    public String lastName;
+}
---
+++ app/src/main/java/com/yiptv/sam/capitalone/Service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.yiptv.sam.capitalone.Service;
+
+/**
+ * Created by Sam on 1/23/16.
+ */
+public class CustomerService {
+
+
+
+}
